ed6e2b2e270d2dabf8ee6dde9f6d28f6f0aa81d4
kim-sardine <kim.sardine@gmail.com>
2021-05-16 22:12:11 +0900

    Add button, Remove toolbar

--- src/components/WadmTable.tsx
+++ src/components/WadmTable.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
-import { createStyles, lighten, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import Box from '@material-ui/core/Box';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import FilterListIcon from '@material-ui/icons/FilterList';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 interface Candidate {
     name: string;
@@ -125,8 +121,8 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             <TableRow>
                 <TableCell
                     key={-1}
-                    align={'center'}
-                    padding={'default'}
+                    align="center"
+                    padding="default"
                     sortDirection={orderBy === -1 ? order : false}
                 >
                     <TableSortLabel
@@ -144,8 +140,8 @@ function EnhancedTableHead(props: EnhancedTableProps) {
                 {candidates.map((candidate, index) => (
                     <TableCell
                         key={index}
-                        align={'right'}
-                        padding={'default'}
+                        align="right"
+                        padding="default"
                         sortDirection={orderBy === index ? order : false}
                     >
                         <TableSortLabel
@@ -156,58 +152,18 @@ function EnhancedTableHead(props: EnhancedTableProps) {
                             {candidate.name}
                             {orderBy === index ? (
                                 <span className={classes.visuallyHidden}>
-                                {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                                 </span>
                             ) : null}
                         </TableSortLabel>
+                        <div>
+                        </div>
                     </TableCell>
                 ))}
             </TableRow>
         </TableHead>
   );
 }
-
-const useToolbarStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            paddingLeft: theme.spacing(2),
-            paddingRight: theme.spacing(1),
-        },
-        highlight:
-            theme.palette.type === 'light'
-                ? {
-                    color: theme.palette.secondary.main,
-                    backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-                }
-                : {
-                    color: theme.palette.text.primary,
-                    backgroundColor: theme.palette.secondary.dark,
-                },
-        title: {
-            flex: '1 1 100%',
-        },
-    }),
-);
-
-interface EnhancedTableToolbarProps {
-}
-
-const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
-    const classes = useToolbarStyles();
-
-    return (
-        <Toolbar className={classes.root}>
-            <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-                Nutrition
-            </Typography>
-            <Tooltip title="Filter list">
-                <IconButton aria-label="filter list">
-                <FilterListIcon />
-                </IconButton>
-            </Tooltip>
-        </Toolbar>
-    );
-};
 
 interface MyTableRowProps {
     category: Category,
@@ -233,7 +189,7 @@ const MyTableRow = (props: MyTableRowProps) => {
 
 const MyTotalRow = () => {
     return (
-        <TableRow hover key={"total"}>
+        <TableRow hover key="total">
             <TableCell align="center">
                 Total
             </TableCell>
@@ -287,7 +243,6 @@ export default function WadmTable() {
     const classes = useStyles();
     const [order, setOrder] = useState<Order>('asc');
     const [orderBy, setOrderBy] = useState<number>(-1);
-    const [dense, setDense] = useState(false);
 
     const handleRequestSort = (event: React.MouseEvent<unknown>, property: number) => {
         const isAsc = orderBy === property && order === 'asc';
@@ -295,45 +250,43 @@ export default function WadmTable() {
         setOrderBy(property);
     };
 
-    const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDense(event.target.checked);
-    };
 
     return (
         <div className={classes.root}>
-        <Paper className={classes.paper}>
-            <EnhancedTableToolbar />
-            <TableContainer>
-                <Table
-                    className={classes.table}
-                    aria-labelledby="tableTitle"
-                    size={dense ? 'small' : 'medium'}
-                    aria-label="enhanced table"
-                >
-                    <EnhancedTableHead
-                        classes={classes}
-                        order={order}
-                        orderBy={orderBy}
-                        onRequestSort={handleRequestSort}
-                    />
-                    <TableBody>
-                        {
-                            stableSort(categories, getComparator(order, orderBy))
-                                .map((category) => { // TODO: Implement Sorting
-                                    return (
-                                        <MyTableRow category={category} />
-                                    );
-                                })
-                        }
-                        <MyTotalRow />
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
-        <FormControlLabel
-            control={<Switch checked={dense} onChange={handleChangeDense} />}
-            label="Dense padding"
-        />
+            <Paper className={classes.paper}>
+                <TableContainer>
+                    <Table
+                        className={classes.table}
+                        aria-labelledby="tableTitle"
+                        size="medium"
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                        />
+                        <TableBody>
+                            {
+                                stableSort(categories, getComparator(order, orderBy))
+                                    .map((category) => { // TODO: Implement Sorting
+                                        return (
+                                            <MyTableRow category={category} />
+                                        );
+                                    })
+                            }
+                            <MyTotalRow />
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+            <Box textAlign='center'>
+                <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
+                    <Button>Add Candidate</Button>
+                    <Button>Add Category</Button>
+                </ButtonGroup>
+            </Box>
         </div>
     );
 }
